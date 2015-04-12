d862133f3d2419c64ea9183f0cda55b94fbf2474
dtimes6 <dtimes6@gmail.com>
2015-04-13 02:24:53 +0800

    添加单元测试环境

---
+++ Test/run.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+var fs = require("fs");
+
+console.log("MDL Unit Test Begin ...\n");
+var path = arguments[4];
+function findAndTest(path) {
+    'use strict';
+    fs.readdir(path, function (err, files) {
+        files.forEach(function (handle) {
+            var file = path + '/' + handle;
+            fs.stat(file, function(err, stat) {
+                if (stat.isDirectory()) {
+                    findAndTest(file);
+                } else if (handle.match(/^case_[^\.]*\.js$/)){
+                    var exec = require('child_process').exec;
+                    exec("cd " + path + ";node " + file, function (err, stdout, stderr) {
+                        if (err) {
+                            console.log("Error calling '" + file + "'");
+                        } else {
+                            fs.readFile(file + ".cmp", 'utf-8', function (err, data) {
+                                if (data !== (stderr + stdout)) {
+                                    fs.writeFile(file + ".log", (stderr + stdout), 'utf-8', function (err) {
+                                        console.log('Running ' + handle + ' ... '  + 'Failed!');
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        });
+    });
+}
+
+findAndTest(path);
