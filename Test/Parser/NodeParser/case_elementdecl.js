a6295d8c62fadcbf3be90ab8aef03806113a6f4d
dtimes6 <dtimes6@gmail.com>
2015-04-19 01:29:58 +0800

    parseElementDecl

---
+++ Test/Parser/NodeParser/case_elementdecl.js
@@ -0,0 +1,8 @@
+var ATPParser = require('../../Compiler/Parser/nodeparser.js');
+require('../../test.js')(__filename, function (data) {
+    'use strict';
+    var parser = new ATPParser();
+    parser.createTokenProvider(data);
+    var node = parser.parseElementDecl(data);
+    console.log(node);
+});
