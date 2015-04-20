986ce4a93909dea9c795c13a8d3f6c6e3b4594ac
dtimes6 <dtimes6@gmail.com>
2015-04-21 02:06:49 +0800

    add stmt for language text

--- Test/Parser/NodeParser/case_language.js
+++ Test/Parser/NodeParser/case_language.js
@@ -5,4 +5,6 @@ require('../../test.js')(__filename, function (data) {
     parser.parse(data);
     console.log(parser.root.childs.stmts);
     console.log(parser.root.childs.stmts[0]);
+    console.log(parser.root.childs.stmts[0].childs.stmt.childs.stmts[0]);
+    console.log(parser.root.childs.stmts[0].childs.stmt.childs.stmts[0].childs.params);
 });
