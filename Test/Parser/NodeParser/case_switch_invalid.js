889565e2ec33f9abd731bab2ee837edb451258b3
dtimes6 <dtimes6@gmail.com>
2015-04-24 15:11:13 +0800

    coverage

---
+++ Test/Parser/NodeParser/case_switch_invalid.js
@@ -0,0 +1,6 @@
+var ATPParser = require('../../Compiler/Parser/nodeparser.js');
+require('../../test.js')(__filename, function (data) {
+    'use strict';
+    var parser = new ATPParser();
+    parser.parse(data);
+});
