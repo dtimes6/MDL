7ecc0f20203ccf4d9f8d7ca9f0e56c9eeb89cc4e
dtimes6 <dtimes6@gmail.com>
2015-05-09 13:59:59 +0800

    enhance test case

--- Test/Parser/Elaboration/case_expand_function.js
+++ Test/Parser/Elaboration/case_expand_function.js
@@ -3,12 +3,14 @@ require('../../test.js')(__filename, function (data) {
     'use strict';
     var parser = new ATPParser();
     parser.parse(data);
-    var template = parser.root.childs.stmts[1];
-    var expand = template.templateExpanding([parser.root.childs.stmts[0]]);
+    var template = parser.root.childs.stmts[2];
+    var expand = template.templateExpanding([parser.root.childs.stmts[0], parser.root.childs.stmts[1]]);
 
     console.log(template.scope.type['BUS']);
     console.log(template.scope.type['BUS'].childs.ref);
+    console.log(template.scope.symbol['N'].childs.ref);
     console.log("----------------------------------");
     console.log(expand.scope.type['BUS']);
     console.log(expand.scope.type['BUS'].childs.ref);
+    console.log(expand.scope.symbol['N'].childs.ref);
 });
